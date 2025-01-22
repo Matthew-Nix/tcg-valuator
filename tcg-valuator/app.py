b58edef7e2dbcc2435d320a1130cb88a29d0ff7b
Matthew Nix <fluff.gfg@outlook.com>
2025-01-22 15:10:39 -0800

    Added basic request test that calls the Scryfall API

--- tcg-valuator/app.py
+++ tcg-valuator/app.py
@@ -1,3 +1,5 @@
+import requests
+
 from flask import Flask
 
 # Create an instance of the Flask class that is the WSGI application.
@@ -9,11 +11,21 @@ app = Flask(__name__)
 # To add other resources, create functions that generate the page contents
 # and add decorators to define the appropriate resource locators for them.
 
+# [moved code lower]
+
+# need to branch out into classes
+
+# API endpoint
+url = "https://api.scryfall.com/cards/named?exact=lightning+bolt "
+
+# GET request to the API
+response = requests.get(url)
+
 @app.route('/')
 @app.route('/hello')
 def hello():
    # Render the page
-   return "Hello Python!"
+   return "Response: " + str(response.status_code)
 
 if __name__ == '__main__':
    # Run the app server on localhost:4449
