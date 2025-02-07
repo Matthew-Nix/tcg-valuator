06dc17f29c19d95132dc5303403b4b6e6b9baf21
Matthew Nix <fluff.gfg@outlook.com>
2025-02-07 15:47:24 -0800

    Rebuilt code using tkinter instead of Flet. Now there's a basic window that takes input and outputs a modified result

--- tcg-valuator/main.py
+++ tcg-valuator/main.py
@@ -1,48 +1,10 @@
-import flet
-from flet import *
-from app_layout import *
-from sidebar import *
+from tkinter import *
+from tkinter import ttk
 
-class Valuator(AppLayout):
-    def __init__(self, page: Page):
-        self.page = page
-        self.appbar_items = [
-            PopupMenuItem(text="Login"),
-            PopupMenuItem(),  # divider
-            PopupMenuItem(text="Settings")
-        ]
-        self.appbar = AppBar(
-            leading=Icon(Icons.GRID_GOLDENRATIO_ROUNDED),
-            leading_width=100,
-            title=Text("Valuator",size=32, text_align="start"),
-            center_title=False,
-            toolbar_height=75,
-            bgcolor=Colors.LIGHT_BLUE_ACCENT_700,
-            actions=[
-                Container(
-                    content=PopupMenuButton(
-                        items=self.appbar_items
-                    ),
-                    margin=margin.only(left=50, right=25)
-                )
-            ],
-        )
-        self.page.appbar = self.appbar
-        self.page.update()
-        super().__init__(
-            self,
-            self.page,
-        )
+from valuator import *
 
-if __name__ == "__main__":
- 
-    def main(page: Page):
- 
-        page.title = "Flet Trello clone"
-        page.padding = 0
-        page.bgcolor = Colors.BLUE_GREY_200
-        app = Valuator(page)
-        page.add(app)
-        page.update()
- 
-    flet.app(main, view=flet.WEB_BROWSER)
+from feetometers import *
+
+root = Tk()
+Valuator(root)
+root.mainloop()
